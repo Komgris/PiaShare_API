9d974b308654904e9e09dc338054fc38689f68bf
devspsec <devs.psec@gmail.com>
2020-02-20 10:43:26 +0700

    Add jwt , Hapi joi

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const dotenv = require('dotenv')
 const mongoose = require('mongoose');
 const authRoute = require('./routes/auth')
+const postRoute = require('./routes/post');
+const cors = require('cors');
+
 dotenv.config();
 
 mongoose.connect(process.env.DB_CONNECT,{ useUnifiedTopology: true },() => 
@@ -10,10 +13,12 @@ mongoose.connect(process.env.DB_CONNECT,{ useUnifiedTopology: true },() =>
 );
 
 //Middleware
+app.use(cors())
 app.use(express.json());
 
 //Route Middleware
 app.use('/api/user', authRoute)
+app.use('/api/post', postRoute)
 
 app.listen(4200, () => console.log('Server Run'));
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../model/UserModel');
-const {registerValidation} = require('../validate/validation');
+const jwt = require('jsonwebtoken');
+const {registerValidation ,logInValidation } = require('../validate/validation');
 const bcrypt = require('bcryptjs');
 
 router.post('/register', async(req,res)=>{
@@ -16,20 +17,46 @@ router.post('/register', async(req,res)=>{
 
     //Hash passwoed
     const salt = await bcrypt.genSalt(10);
-    const hashPassword = await bcrypt.hash(req.body.password, salt);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
     //Create new user
     const user = new User({
         username: req.body.username,
-        password: req.body.password
+        password: hashedPassword
     });
     try{
         const savedUser =  await user.save();
-        res.json(savedUser);
+        res.send({user : user._id});
     }
     catch(err){
         res.status(400).send(err);
     }
 
+});
+
+//LOGIN ******************************************************************************************************************
+router.post('/login', async(req,res)=>{
+    const {error} = logInValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+        //Check if exist
+        const usernameExist = await User.findOne({
+            username:req.body.username
+        })
+        if(!usernameExist) return res.status(400).send('Invalid Username or Password');
+
+        const validPass = await bcrypt.compare(
+            req.body.password, 
+            usernameExist.password
+            );
+        if(!validPass)return res.status(400).send('Invalid Username or Password');
+
+        //Create Token
+        const token = jwt.sign({_id: usernameExist._id},process.env.TOKEN);
+        res.header('auth-token',token).send(token);
+
+        res.send('Logged in');
+    
 });
 
 module.exports = router;
