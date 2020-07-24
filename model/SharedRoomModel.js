23775fa39eae4599c1db88337decf75546509da7
devspsec <devs.psec@gmail.com>
2020-07-24 11:58:24 +0700

    Add shared room schema

---
+++ model/SharedRoomModel.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+const sharedRoomSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        min:3,
+        max:255
+    },
+    budget:{
+        type: Number,
+        required: true,
+        min:1
+    },
+    member:{
+        type:Number,
+        required:true,
+        min:1
+    },
+    peroid:{
+        type:Number,
+        required:true,
+        min:1
+    },
+    start:{
+        type:Date,
+        required:true
+    },
+    finish:{
+        type:Date,
+        required:true
+    }
+
+
+
+
+})
+module.exports = mongoose.model('SharedRoom',sharedRoomSchema)
